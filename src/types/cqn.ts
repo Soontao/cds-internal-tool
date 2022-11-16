7f006ec7bf52e049acf52548675c85fededbd60c
Theo Sun <theo.sun@outlook.com>
2022-11-16 10:21:04 +0800

    chore: type update

--- src/types/cqn.ts
+++ src/types/cqn.ts
@@ -3,14 +3,7 @@ import { expr, ref, xpr, _xpr } from "./cxn";
 /**
  * CDS Query Notation
  */
-export type CQN = {
-  SELECT?: SELECT,
-  INSERT?: INSERT,
-  UPDATE?: UPDATE,
-  DELETE?: DELETE,
-  CREATE?: CREATE,
-  DROP?: DROP,
-}
+export type CQN = Query
 
 export type Query = Partial<SELECT & INSERT & UPDATE & DELETE & CREATE & DROP>
 
@@ -18,7 +11,7 @@ export type ParsedExpr = expr & { _: string }
 
 export type SELECT = {
   SELECT: {
-    distinct?: true
+    distinct?: boolean
     one?: boolean
     from: source
     columns?: column_expr[]
@@ -28,16 +21,21 @@ export type SELECT = {
     groupBy?: expr[]
     orderBy?: ordering_term[]
     limit?: { rows: number, offset: number }
+    search?: _xpr;
+    forUpdate?: { wait: number }
+    forShareLock?: { wait: number }
+    count?: boolean
   }
 }
 
 export type INSERT = {
   INSERT: {
     into: name
-    entries: any[]
     columns: string[]
     values: any[]
     rows: any[]
+    entries: any[]
+    as?: SELECT;
   }
 }
 
@@ -59,19 +57,21 @@ export type DELETE = {
 export type CREATE = {
   CREATE: {
     entity: name
+    as?: SELECT
   }
 }
 
 export type DROP = {
   DROP: {
-    entity: name
+    table?: ref
+    view?: ref
+    entity?: ref
   }
 }
 
-type name = string
+type name = string | ref
 type source = (ref | SELECT) & { as?: name, join?: name, on?: xpr }
-export type column_expr = expr & { as?: name, cast?: any, expand?: column_expr[], inline?: column_expr[] }
-export type predicate = _xpr
 type ordering_term = expr & { asc?: true, desc?: true }
 
-
+export type column_expr = expr & { as?: name, cast?: any, expand?: column_expr[], inline?: column_expr[] }
+export type predicate = _xpr
